e4067b860d982b125f2c386e530d85594dab37d5
Adam Brock <a.paulbrock@gmail.com>
2022-06-25 19:08:15 -0400

    Finished up the styling.

--- public/js/blog-page.js
+++ public/js/blog-page.js
@@ -2,20 +2,22 @@ const newCommentHandler = async (evt) => {
   evt.preventDefault();
   const content = document.querySelector('#comment-text-area').value;
   const post_id = evt.target.getAttribute('data-id');
-  console.log(content);
-  console.log(post_id);
-  const response = await fetch('/api/comments', {
-    method: 'POST',
-    body: JSON.stringify({
-      post_id,
-      content,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
-  if (response.ok) {
-    document.location.reload();
+  if (content) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        post_id,
+        content,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert('Comment failed to post!');
+    }
   } else {
-    alert('Comment failed to post!');
+    alert('Use your words!');
   }
 };
 
--- public/js/create-blog.js
+++ public/js/create-blog.js
@@ -2,17 +2,21 @@ const newPostHandler = async (evt) => {
   evt.preventDefault();
   const title = document.querySelector('#create-post-title').value;
   const content = document.querySelector('#create-post-content').value;
+  if (title && content) {
+    await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-  await fetch('/api/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-      title,
-      content,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-  });
-
-  document.location.replace('/dashboard');
+    document.location.replace('/dashboard');
+  }
+  else {
+    alert("You need to give your post a title and some content.")
+  }
 };
 
 document
